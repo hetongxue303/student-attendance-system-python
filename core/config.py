9d3557ed5cdd4f53243a2a32c2fa2e7db703bcc0
hetongxue303 <1974597686@qq.com>
2022-11-16 14:55:08 +0800

    添加全局配置

---
+++ core/config.py
@@ -0,0 +1,43 @@
+"""
+全局配置
+@Author:何同学
+"""
+import secrets
+from typing import List
+from pydantic import BaseSettings, AnyHttpUrl
+
+app_desc = """
+    🎉 用户登录 🎉
+    ✨ 账号: admin ✨
+    ✨ 密码: 123456 ✨
+"""
+
+
+class Settings(BaseSettings):
+    """应用配置"""
+    APP_TITLE: str = '学生考勤系统'  # 应用标题
+    APP_API_PREFIX: str = '/api'  # 接口前缀
+    APP_DEBUG: bool = True  # 是否debug
+    APP_CORS: bool = True  # 是否跨域
+    APP_DESC: str = app_desc  # 项目描述
+    APP_VERSION: str = '0.0.1'  # 版本
+    APP_STATIC_DIR: str = 'static'  # 静态文件目录
+    APP_GLOBAL_ENCODING: str = 'utf-8'  # 全局编码
+    APP_CORS_ORIGINS: List[AnyHttpUrl] = ['http://127.0.0.1:3000', 'http://127.0.0.1:5179']  # 跨域请求(列表)
+    APP_IS_RELOAD: bool = True  # 是否热部署
+
+    """数据源配置"""
+    REDIS_URI: str = 'redis://:123456@127.0.0.1:6379/1'  # redis
+    DATABASE_URI: str = 'mysql+pymysql://root:123456@127.0.0.1:3306/student_attendance_system?charset=utf8'  # mysql
+    DATABASE_ECHO: bool = False  # 是否打印数据库日志 (可看到创建表、表数据增删改查的信息)
+
+    """JWT配置"""
+    JWT_SECRET_KEY: str = secrets.token_urlsafe(32)  # 密钥(每次重启服务密钥都会改变, token解密失败导致过期, 可设置为常量)
+    JWT_EXPIRE: int = 24 * 60 * 60 * 1000  # token过期时间: 60 minutes * 24 hours * 1 days = 1 days
+
+    class Config:
+        env_fil: str = '.env'
+        case_sensitive: bool = True  # 区分大小写
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+"""
+程序入口
+@Author:何同学
+"""
 from fastapi import FastAPI
 
-app = FastAPI()
+from core.config import settings
+
+app = FastAPI(
+    title=settings.APP_TITLE,
+    description=settings.APP_DESC,
+    version=settings.APP_VERSION,
+    debug=settings.APP_DEBUG,
+    openapi_url=f'{settings.APP_API_PREFIX}/openapi.json'
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get('/test', summary='测试接口')
+async def test():
+    return '测试成功'
